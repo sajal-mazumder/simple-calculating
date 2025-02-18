e81e665cfca1acc14d4b1b5c1c2b70672146e540
sajal-mazumder <sajalmazumder47@gmail.com>
2025-02-18 20:44:41 +0600

    some update

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 
 
-document.getElementById("num1")
-document.getElementById("num2")
+// document.getElementById("num1")
+// document.getElementById("num2")
 
-console.log(document.getElementById("num1").value);
+// console.log(document.getElementById("num1").value);
+
+// js comjparison operators
+
+// == is equal to value
+// === is equal to value and type
+// != is not equal to value
+// !== is not equal to value and type
+// <,> is less than and greater than
+// =<, >= is less than equal and greater than equal
+// ? ternary operator
+
+let x = "sajal";
+let y = "sajal";
+
+console.log(x == y);
+console.log(x != y);
+
+console.log(x === y);
+console.log(x !== y);
+
+console.log(x > y);
+console.log(x < y);
+
+console.log(x >= y);
+console.log(x <= y);
+
+
+
+
+
+
+
+

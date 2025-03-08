dd3c0172e7d4fc7a1a1345e1ad97aa309096850b
sajal-mazumder <sajalmazumder47@gmail.com>
2025-03-08 19:33:17 +0600

    some update

--- script.js
+++ script.js
@@ -15,20 +15,37 @@
 // =<, >= is less than equal and greater than equal
 // ? ternary operator
 
-let x = "sajal";
-let y = "sajal";
+// let x = "sajal";
+// let y = "sajal";
 
-console.log(x == y);
-console.log(x != y);
+// console.log(x == y);
+// console.log(x != y);
 
-console.log(x === y);
-console.log(x !== y);
+// console.log(x === y);
+// console.log(x !== y);
 
-console.log(x > y);
-console.log(x < y);
+// console.log(x > y);
+// console.log(x < y);
 
-console.log(x >= y);
-console.log(x <= y);
+// console.log(x >= y);
+// console.log(x <= y);
+
+// &&	and	(x < 10 && y > 1) is true	
+// ||	or	(x == 5 || y == 5) is false	
+// !	not	!(x == y) is true
+
+// let x = 5;
+// let y = 4;
+
+// console.log(x < 12 && y >3);
+// console.log(x == 5 || y == 4);
+// console.log(!(x == y));
+
+function myAge() {
+    let age = document.getElementById("age").value;
+    let voteable = (age < 18) ? "Too young":"Old enough";
+    document.getElementById("demo").innerHTML = voteable + " to vote.";
+  }
 
 
 
